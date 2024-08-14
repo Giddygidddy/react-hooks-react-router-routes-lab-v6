8c0f126a4b0fc43e1125114459ccf2c2b232769c
Zilla <gideon.kariuki@student.moringaschool.com>
2024-08-14 17:23:30 +0300

    completed lab

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,29 @@
-import { useEffect, useState } from "react";
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const directors = [
+  { name: 'Director 1', movies: ['Movie A', 'Movie B'] },
+  { name: 'Director 2', movies: ['Movie C', 'Movie D'] },
+  // Add more directors as needed
+];
 
 function Directors() {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Director info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <article key={index}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map((movie, idx) => (
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </div>
   );
-};
+}
 
 export default Directors;
